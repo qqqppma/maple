42d182a1c8e39a6d6c6f3a2fa06e3f3fc0c08099
seungho <qqqppma@gmail.com>
2025-04-22 10:13:23 +0900

    up

--- app2.py
+++ app2.py
@@ -320,6 +320,8 @@ if "user" in st.session_state:
     st.sidebar.markdown(f"👤 로그인: {st.session_state['nickname']}")
     if st.sidebar.button("로그아웃"):
         user_id = st.session_state.get("user")
+        nickname = st.session_state["nickname"]
+        is_admin = st.session_state["is_admin"]
         if user_id:
             # ✅ 토큰 무효화
             supabase.table("Users").update({"login_token": None}).eq("user_id", user_id).execute()
