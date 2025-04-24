c24f29b8719f73c1b16509248d2ffe4f96e79ac5
seungho <qqqppma@gmail.com>
2025-04-24 12:09:32 +0900

    up

--- app2.py
+++ app2.py
@@ -771,7 +771,7 @@ elif menu == "악마길드 길컨관리":
             use_container_width=True,
             disabled=["닉네임"],
             num_rows="dynamic",
-            height=180,
+            height=200,
             key="main_editor"
         )
 
