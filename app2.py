10654f0e2b881a6ce3449dc05c1aab8ec47c803b
seungho <qqqppma@gmail.com>
2025-05-19 12:11:24 +0900

    up

--- app2.py
+++ app2.py
@@ -924,8 +924,8 @@ if menu == "악마 길드원 정보 등록":
 
 
                     
-elif menu == "악마길드 길어쿠롤 관리":
-    st.subheader("👥 악마길드 길드쿠롤 관리")
+elif menu == "악마길드 길컨 관리":
+    st.subheader("👥 악마길드 길컨 관리")
 
     mainmembers = get_mainmembers()
     members = get_members()
